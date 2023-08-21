0cfe426f309b5faf64854b44a656948b28e0eef5
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-08-21 11:52:50 -0400

    imported pizzastore in regularpizza component

--- src/components/RegularPizza.vue
+++ src/components/RegularPizza.vue
@@ -1,15 +1,32 @@
 <template>
-    <div>
-
+  <div v-for="pizzaItem in pizzaList" :key="pizzaItem.id">
+    <div v-if="pizzaItem.custom === false">
+      <h2>{{ pizzaItem.title }}</h2>
+      <p><img :src="pizzaItem.image" alt="Pizza image" /></p>
+      <p>{{ pizzaItem.description }}</p>
+      <p>Size:</p>
+      <p>Small: {{ pizzaItem.size.small }}</p>
+      <p>Medium: {{ pizzaItem.size.medium }}</p>
+      <p>Large: {{ pizzaItem.size.large }}</p>
+      <p>X-Large: {{ pizzaItem.size.xlarge }}</p>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import { usePizzaStore } from "@/store/PizzaStore";
+// import { storeToRefs } from "pinia";
+
+export default {
+  name: "RegularPizza",
+  setup() {
+    const pizzaStore = usePizzaStore();
+
+    pizzaStore.fetchPizza();
+    console.log("olha aqui", pizzaStore.pizza);
+    return { pizzaList: pizzaStore.pizza };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>

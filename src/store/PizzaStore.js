43ed199e6b78b0810621d7c25164f5551fe2ca22
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-08-21 11:52:15 -0400

    created fetch to PizzaStore

--- src/store/PizzaStore.js
+++ src/store/PizzaStore.js
@@ -1,26 +1,28 @@
 import { defineStore } from "pinia";
 
-export const usePizzaStore = defineStore('pizza',{
+export const usePizzaStore = defineStore("pizzaStore", {
   // state = data
   state: () => ({
-    pizza:[]
+    pizza: [],
   }),
   // getters = computed
-//   getters: {
-//     doubleCount() {
-//       return this.count * 2;
-//     },
-//   },
+  //   getters: {
+  //     doubleCount() {
+  //       return this.count * 2;
+  //     },
+  //   },
   // actions = methods
   actions: {
     async fetchPizza() {
-        try{ 
-            const response = await fetch('http://localhost:3000/pizza');
-            this.pizza = await response.json();
-            return this.pizza;
-        } catch (error) {
-            console.log(error);
-        }
-    }
+      try {
+        const response = await fetch("http://localhost:3000/pizza");
+
+        const data = await response.json();
+        this.pizza = data;
+        return data;
+      } catch (error) {
+        console.log("Error fetching pizza:", error);
+      }
+    },
   },
 });

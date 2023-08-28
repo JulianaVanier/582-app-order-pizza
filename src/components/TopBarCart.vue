35dd3eb96c81410226ae0b1850791bb12a8a9d25
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-08-28 14:08:29 -0400

    created new component to topbar Cart

--- src/components/TopBarCart.vue
+++ src/components/TopBarCart.vue
@@ -1,40 +1,25 @@
 <template>
-  <!-- <section class="top">
+  <section class="top">
     <div class="back-btn">
       <router-link to="/">
         <img src="/img/back-icon.png" alt="Back icon" />
       </router-link>
     </div>
-    <div class="title">
-      <h1>Cart</h1>
-    </div>
     <div class="cart-btn">
       <div class="cart">
         <img src="/img/cart-icon.png" alt="Cart icon" />
+        <!-- <p>{{ itemsCart }}</p> -->
         <div class="qt-cart">{{ pizzaCart.count }}</div>
       </div>
     </div>
-  </section> -->
-  <TopBarCart></TopBarCart>
-  <CartComponent></CartComponent>
+  </section>
 </template>
 
 <script>
 import { usePizzaStore } from "@/store/PizzaStore";
-import CartComponent from "@/components/CartComponent.vue";
-import TopBarCart from "@/components/TopBarCart.vue";
 
 export default {
-  name: "CartView",
-  data() {
-    return {
-      itemsCart: [],
-    };
-  },
-  components: {
-    CartComponent,
-    TopBarCart,
-  },
+  name: "TopBarCart",
   setup() {
     const pizzaCart = usePizzaStore();
     return { pizzaCart };

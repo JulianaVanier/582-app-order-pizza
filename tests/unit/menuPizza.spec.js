b81cc265f82cbba79aeff34a4594bee440041637
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-09-03 23:21:35 -0400

    added some tests in PizzaItem

--- tests/unit/menuPizza.spec.js
+++ tests/unit/menuPizza.spec.js
@@ -1,8 +1,54 @@
+
+// import { createTestingPinia } from '@pinia/testing';
+// import { mount } from '@vue/test-utils';
 // import { shallowMount } from "@vue/test-utils";
+// import { usePizzaStore } from '@/store/PizzaStore';
 // import MenuPizza from "@/components/MenuPizza.vue";
 
-// describe("MenuPizza.vue", () => {
-//   it("loop displaying regular pizza's", () => {});
+// const wrapper = mount(MenuPizza, {
+//     global: {
+//       plugins: [createTestingPinia()],
+//     },
+//   })
+// console.log("WRAPER", wrapper);
+// const store = usePizzaStore() // uses the testing pinia!
 
-//   it("emit event when clicked Add to Cart", () => {});
+// const pizza = {
+//     id: 1,
+//     custom: false,
+//     title: "Pizza 1",
+//     description: "Pizza 1 description",
+//     size:{
+//         small: 10,
+//         medium: 20,
+//         large: 30,
+//     },
+//     image: "pizza1.png",
+// };
+
+
+
+// store.addPizza(pizza);
+// expect(store.getPizzas).toHaveBeenCalledTimes(1)
+
+// actions are stubbed by default, meaning they don't execute their code by default.
+// See below to customize this behavior.
+// store.someAction()
+
+// expect(store.someAction).toHaveBeenCalledTimes(1)
+// expect(store.someAction).toHaveBeenLastCalledWith()
+
+// describe("MenuPizza.vue", async () => {
+//     // beforeEach(() => {
+//     //     setActivePinia(createPinia());
+//     //   });
+
+//   it("loop displaying pizza's", () => {
+//     const wrapper = shallowMount(MenuPizza);
+//     const pizzas = wrapper.findAll(".pizza");
+
+//     expect(pizzas.length).toBe(3);
+//   });
+
+// //   it("emit event when clicked Add to Cart", () => {});
 // });
--- tests/unit/pizzaItem.spec.js
+++ tests/unit/pizzaItem.spec.js
@@ -1,48 +1,153 @@
 // import { shallowMount } from "@vue/test-utils";
 // import PizzaItem from "@/components/PizzaItem.vue";
+// import { createTestingPinia } from '@pinia/testing';
+// import { mount } from '@vue/test-utils';
+// import { usePizzaStore } from '@/store/PizzaStore';
 
 // describe("PizzaItem.vue", () => {
-//     it("renders props.pizza when passed", () => {
-//         const pizza = {
-//             id: 1,
-//             custom: false,
-//             title: "Pizza 1",
-//             description: "Pizza 1 description",
-//             size:{
-//                 small: 10,
-//                 medium: 20,
-//                 large: 30,
+//     it("renders and display props tasks when passed", () => {
+//         const wrapper = mount(PizzaItem, {
+//             global: {
+//               plugins: [createTestingPinia()],
 //             },
-//             image: "pizza1.png",
-//         };
-//         const wrapper = shallowMount(PizzaItem, {
-//             props: { pizza },
-//         });
-//         expect(wrapper.find("h2").text()).toBe(tasks.title);
-//     });
-//     it('toggles selectSize property when selectSizeRun method is called', async () => {
-//         const pizza = {
-//             id: 1,
-//             custom: false,
-//             title: "Pizza 1",
-//             description: "Pizza 1 description",
-//             size:{
-//                 small: 10,
-//                 medium: 20,
-//                 large: 30,
-//             },
-//             image: "pizza1.png",
-//         };
+//           })
+//         expect(wrapper.find("h2").text()).toBe(pizza.title);
+//     }
+//     );
+// });
 
-//         const wrapper = shallowMount(PizzaItem, {
-//             props: { pizza, selectSize: false }
 
-//         });
+// // const store = usePizzaStore() // uses the testing pinia!
 
-//         expect(wrapper.vm.selectSize).toBe(false);
+// // const pizza = {
+// //     id: 1,
+// //     custom: false,
+// //     title: "Pizza 1",
+// //     description: "Pizza 1 description",
+// //     size:{
+// //         small: 10,
+// //         medium: 20,
+// //         large: 30,
+// //     },
+// //     image: "pizza1.png",
+// // };
 
-//         await wrapper.vm.selectSizeRun('1');
+// // sendingToCart(pizza) {
+// //     // returning the id of the pizza to customize to function sendingToCart in PizzaItem
+// //     store.addPizzaToCart(pizza);
+// // }
 
-//         expect(wrapper.vm.selectSize).toBe(true);
-//       });
+// // expect(store.addPizzaToCart).toHaveBeenCalled();
+
+
+// describe("PizzaItem.vue", () => {
+//     // it("display Ingredient in Pizza when ingredient._id = true", () => {
+   
+//     // });
+
+//     // it("display Ingredient in Pizza when ingredient._id = false", () => {
+   
+//     // });
+//     // it("renders and display props tasks when passed", () => {
+//     //     const pizza = {
+//     //         id: 1,
+//     //         custom: false,
+//     //         title: "Pizza 1",
+//     //         description: "Pizza 1 description",
+//     //         size:{
+//     //             small: 10,
+//     //             medium: 20,
+//     //             large: 30,
+//     //         },
+//     //         image: "pizza1.png",
+//     //     };
+//     //     const wrapper = shallowMount(PizzaItem, {
+//     //         props: { pizza },
+//     //     });
+//     //     expect(wrapper.find("h2").text()).toBe(pizza.title);
+
+//     //     });
+
+//     // it("display btn Add to cart when pizza.cutom = false && displayBtnCart===true", () => {
+//     //     const pizza = {
+//     //         id: 1,
+//     //         custom: false,
+//     //         title: "Pizza 1",
+//     //         description: "Pizza 1 description",
+//     //         size:{
+//     //             small: 10,
+//     //             medium: 20,
+//     //             large: 30,
+//     //         },
+//     //         image: "pizza1.png",
+//     //     };
+//     //     const wrapper = shallowMount(PizzaItem, {
+//     //         props: { pizza, displayBtnCart: true },
+//     //     });
+//     //     expect(wrapper.find(".btn").exists()).toBe(true);
+//     // });
+
+//     // it("display btn customize when pizza.cutom = true && this.$route.name === 'index'", () => {
+//     //     const pizza = {
+//     //         id: 1,
+//     //         custom: true,
+//     //         title: "Pizza 1",
+//     //         description: "Pizza 1 description",
+//     //         size:{
+//     //             small: 10,
+//     //             medium: 20,
+//     //             large: 30,
+//     //         },
+//     //         image: "pizza1.png",
+//     //     };
+//     //     const wrapper = shallowMount(PizzaItem, {
+//     //         props: { pizza },
+//     //     });
+//     //     expect(wrapper.find(".btn").exists()).toBe(true);
+//     // });
+
+
+//     // it("renders props.pizza when passed", () => {
+//     //     const pizza = {
+//     //         id: 1,
+//     //         custom: false,
+//     //         title: "Pizza 1",
+//     //         description: "Pizza 1 description",
+//     //         size:{
+//     //             small: 10,
+//     //             medium: 20,
+//     //             large: 30,
+//     //         },
+//     //         image: "pizza1.png",
+//     //     };
+//     //     const wrapper = shallowMount(PizzaItem, {
+//     //         props: { pizza },
+//     //     });
+//     //     expect(wrapper.find("h2").text()).toBe(tasks.title);
+//     // });
+//     // it('toggles selectSize property when selectSizeRun method is called', async () => {
+//     //     const pizza = {
+//     //         id: 1,
+//     //         custom: false,
+//     //         title: "Pizza 1",
+//     //         description: "Pizza 1 description",
+//     //         size:{
+//     //             small: 10,
+//     //             medium: 20,
+//     //             large: 30,
+//     //         },
+//     //         image: "pizza1.png",
+//     //     };
+
+//     //     const wrapper = shallowMount(PizzaItem, {
+//     //         props: { pizza, selectSize: false }
+
+//     //     });
+
+//     //     expect(wrapper.vm.selectSize).toBe(false);
+
+//     //     await wrapper.vm.selectSizeRun('1');
+
+//     //     expect(wrapper.vm.selectSize).toBe(true);
+//     //   });
 // });

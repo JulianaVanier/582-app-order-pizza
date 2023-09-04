ee49a28b4cf34b801c3bd5a236379fa83988a7bd
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-09-04 00:00:26 -0400

    tests for ingredientItem

--- tests/unit/RegularPizza.spec.js
+++
@@ -1,8 +0,0 @@
-// import { shallowMount } from "@vue/test-utils";
-// import RegularPizza from "@/components/RegularPizza.vue";
-
-// describe("RegularPizza.vue", () => {
-//   it("Initial rendering is working", async () => {});
-//   it("is the condition applied to the button working", async () => {});
-//   it("are the buttons working properly", async () => {});
-// });
